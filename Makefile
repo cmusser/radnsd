fe5a700f68978b6061e353cfbb9423c60bb02bcd
Chuck Musser <cmusser@sonic.net>
2015-09-30 13:31:01 -0700

    Turn up compiler warnings; remove unneeded header files.

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 CC=gcc
+CFLAGS=-Wall
 
 all: radnsd
 
--- radnsd.c
+++ radnsd.c
@@ -1,21 +1,20 @@
 #include <sys/event.h>
 #include <sys/queue.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <sys/types.h>
+
 #include <arpa/inet.h>
-#include <netinet/in.h>
-#include <netinet/ip6.h>
-#include <netinet6/ip6_var.h>
 #include <netinet/icmp6.h>
+#include <netinet/in.h>
 
 #include <errno.h>
-#include <inttypes.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <syslog.h>
 #include <time.h>
 #include <unistd.h>
